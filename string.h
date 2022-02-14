b3502a8113e3fb7d25f5eb8455fa1c49faf5acc8
Yuhui <mrhuangyuhui@users.noreply.github.com>
2022-02-14 16:35:23 +0800

    Create string.h

---
+++ string.h
@@ -0,0 +1,64 @@
+#ifndef __MYSTRING__
+#define __MYSTRING__
+
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+class String {
+public:
+    explicit String(const char *cstr = 0);
+    
+    ~String();
+
+    String(const String &str);
+
+    String &operator=(const String &str);
+
+    char *get_c_str() const { return m_data; }
+
+private:
+    char *m_data;
+};
+
+inline
+String::String(const char *cstr) {
+    if (cstr) {
+        m_data = new char[strlen(cstr) + 1];
+        strcpy(m_data, cstr);
+    } else {
+        m_data = new char[1];
+        *m_data = '\0';
+    }
+}
+
+inline
+String::~String() {
+    delete[] m_data;
+}
+
+inline
+String::String(const String &str) {
+    m_data = new char[strlen(str.m_data) + 1];
+    strcpy(m_data, str.m_data);
+}
+
+inline String &
+String::operator=(const String &str) {
+    if (this == &str) {
+        return *this;
+    }
+    delete[] m_data;
+    m_data = new char[strlen(str.m_data) + 1];
+    strcpy(m_data, str.m_data);
+    return *this;
+}
+
+inline ostream &
+operator<<(ostream &os, const String &str) {
+    os << str.get_c_str();
+    return os;
+}
+
+#endif
